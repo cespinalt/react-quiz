69dd2ca803410ccb68cf288917f6be0cbec1d39d
Carlos Espinal <cespinalt@gmail.com>
2016-10-09 19:08:15 -0700

    Fix webpack config file
    Add modules key and nexted the loaders array inside of it

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,16 @@ module.exports = {
     path: __dirname,
     filename: 'app/js/bundle.js'
   },
-  loaders: [
-    {
-      loader: 'babel-loader',
-      exclude: 'node_modules',
-      query: {
-        presets: ['es2015', 'react']
-      },
-    }
-  ]
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+        query: {
+          presets: ['es2015', 'react']
+        },
+      }
+    ]
+  }
 }
