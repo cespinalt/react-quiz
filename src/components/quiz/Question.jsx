d5af24d6bb8ee2d312b5045ba7eb8dc5e21bb1c1
Carlos Espinal <cespinalt@gmail.com>
2016-10-09 22:18:55 -0700

    Add classes to the JSX markup

--- src/components/quiz/Question.jsx
+++ src/components/quiz/Question.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import Answer from './Answer.jsx';
 
 class Question extends Component {
   render() {
     return(
-      <div>
-        {this.props.questions.text}
+      <div className="">
+        <h3>{this.props.question.text}</h3>
+        <ul className="list-group">
+          {this.props.question.choices.map(a => {
+            return <Answer answer={a} key={a.id} />
+          })}
+        </ul>
       </div>
     )
   }
