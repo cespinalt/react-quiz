b650a634a6b25fecab6e83048d962cab839f9f96
Carlos Espinal <cespinalt@gmail.com>
2016-10-09 19:01:36 -0700

    Add questions for quiz in the state of the component

---
+++ src/components/App.jsx
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      questions: [
+        {
+          id: 1,
+          text: 'What\'s your father\'s name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'Hamlet',
+            },
+            {
+              id: 'b',
+              text: 'Peter',
+            },
+            {
+              id: 'c',
+              text: 'Pedro',
+            },
+          ]
+        },
+        {
+          id: 2,
+          text: 'What\'s your mather\'s name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'Carmen',
+            },
+            {
+              id: 'b',
+              text: 'Neysa',
+            },
+            {
+              id: 'c',
+              text: 'Jacqueline',
+            },
+          ]
+        },
+        {
+          id: 3,
+          text: 'What\'s your younger brother\'s name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'Hamlet',
+            },
+            {
+              id: 'b',
+              text: 'Luis',
+            },
+            {
+              id: 'c',
+              text: 'Pablo',
+            },
+          ]
+        }
+      ]
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        {this.state.questions.map(q => {
+          return q.text;
+        })}
+      </div>
+    )
+  }
+}
+
+export default App;
